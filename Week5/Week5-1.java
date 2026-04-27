cc9d34265656de85a7a95f7ebc2dd4dec419f542
Anruth <vtu24939@veltech.edu.in>
2026-04-27 22:43:55 +0530

    Create Week5-1.java

---
+++ Week5/Week5-1.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        List<Integer> list1=new ArrayList<>();
+        List<Integer> list2=new ArrayList<>();
+        preorder(list1,p);
+        preorder(list2,q);
+        return (list1.equals(list2));
+    }
+    static void preorder(List<Integer> list,TreeNode root){
+        if(root==null){ 
+            list.add(null);
+            return;
+        }
+        list.add(root.val);
+        preorder(list,root.left);
+        preorder(list,root.right);   
+    }
+}
