b8b05fe9e5b218399e6f2323ea6647cb1c5c14a4
Anruth <vtu24939@veltech.edu.in>
2026-04-28 15:44:58 +0530

    Create Week8-7.java

---
+++ Week8/Week8-7.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isHappy(int n) {
+        int ans=0;
+        while(n>0){
+            int r=n%10;
+            ans+=r*r;
+            n/=10;
+        }
+        if(ans==1) return true;
+        else if(ans==4) return false;
+        return isHappy(ans);
+    }
+}
